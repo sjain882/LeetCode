2820de948f1ac8da115e7eaf21fe42d69d1c612b
sjain882 <43217178+sjain882@users.noreply.github.com>
2025-04-26 17:19:10 +0100

    Setup project

---
+++ LeetCode/CSharp/Challenges/Easy/PalindromeNumber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// https://leetcode.com/problems/palindrome-number/description/
+
+namespace Leetcode.Challenges.Easy
+{
+    internal class PalindromeNumber
+    {
+
+
+
+
+
+
+    }
+}
---
+++ LeetCode/CSharp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Leetcode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
